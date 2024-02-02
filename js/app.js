8b1c80cc77afa5644173ce43d7a099212c59c458
Maksim <adoberoflit@gmail.com>
2024-02-02 15:18:05 +0300

    hidden header

--- js/app.js
+++ js/app.js
@@ -3,15 +3,21 @@ import "https://cdn.jsdelivr.net/npm/simplebar@latest/dist/simplebar.min.js";
 const app = document.querySelector(".app");
 const header = document.querySelector(".header-wrapper");
 
+console.log(app);
 console.log(header);
+let prevState = window.scrollY;
 
-app.addEventListener("scroll", () => {
-	let scrollTop = document.scrollHeight;
-	console.log(scrollTop);
+window.addEventListener("scroll", () => {
+	console.log(prevState, "scroll prevState");
+	let scrollTop = window.scrollY;
+	const scrollDown = prevState < scrollTop;
+	console.log(scrollTop, "scroll scrollTop");
 
-	if (scrollTop >= 100) {
+	if (scrollTop >= 150 && scrollDown) {
 		header.classList.add("hidden");
 	} else {
 		header.classList.remove("hidden");
 	}
+
+	prevState = scrollTop;
 });
